31361aa42930f731d1652b5bbbb90b8aaf482386
Your Name <you@example.com>
2018-01-06 16:07:38 +0000

    feature: Rates can now be read from a cache file (which expires after a week) by default.

--- justexchangerates.gemspec
+++ justexchangerates.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'justexchangerates'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Fetches the latest exchange rates using ' + 
       'the API from http://fixer.io/'
   s.authors = ['James Robertson']
   s.files = Dir['lib/justexchangerates.rb']  
+  s.add_runtime_dependency('chronic_duration', '~> 0.10', '>=0.10.6')
   s.signing_key = '../privatekeys/justexchangerates.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/justexchangerates.rb
+++ lib/justexchangerates.rb
@@ -4,36 +4,79 @@
 
 require 'json'
 require 'open-uri'
+require 'chronic_duration'
 
 
 class JustExchangeRates
 
   attr_reader *%i(base date rates)
 
-  def initialize(api:'https://api.fixer.io/latest?base=', base: 'USD')
+  def initialize(api:'https://api.fixer.io/latest?base=', base: 'USD', 
+                 cache_refresh: '1 week', cache_path: '.', debug: false)
 
-    response = open(api + base.upcase)
+    @url, @base, @debug = api, base, debug
+    
+    filename = 'justexchangerates.json'    
+    @cache_filepath = File.join(cache_path, filename)
+      
+    h = if cache_refresh then      
+            
+      if File.exists? @cache_filepath then
+        
+        h2 = JSON.parse(File.read @cache_filepath)
+        seconds = ChronicDuration.parse(cache_refresh) 
 
-    if response.status.last == "OK" then
-
-      h = JSON.parse(response.read)
-      @base, @date, @rates = h['base'], h['date'], h['rates']
-
+        if (Time.parse(h2['date']) + seconds) < Time.now then
+          fetch_rates
+        else
+          puts 'fetching rates from local cache ...' if @debug
+          h2
+        end
+        
+      else
+        fetch_rates
+      end        
+      
     else
-      puts "JustExchangeRates API error %s : %s" % response.status
+      
+      fetch_rates
+      
     end
+    
+
+    @base, @date, @rates = h['base'], h['date'], h['rates']    
   
   end
 
   def rate(currency)
     @rates[currency.upcase]
   end
+  
+  private
+  
+  def fetch_rates()
+
+    response = open(@url + @base.upcase)
+
+    if response.status.last == "OK" then
+
+      h = JSON.parse(response.read)
+      h['date'] = Time.now
+      File.write @cache_filepath, h.to_json
+      h
+
+    else
+      puts "JustExchangeRates API error %s : %s" % response.status
+      {}
+    end          
+    
+  end
 
 end
 
 if __FILE__ == $0 then
 
-  jer = JustExchangeRates.new(base: 'USB')
+  jer = JustExchangeRates.new(base: 'USD')
   jer.rate(VARGS.first) # e.g. GBP #=> 0.73792
 
 end
